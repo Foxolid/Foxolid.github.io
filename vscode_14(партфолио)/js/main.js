a1d5436f705ad33a7bf82d8baed22a81f2455609
Foxolid <68548585+Foxolid@users.noreply.github.com>
2020-12-03 12:38:15 +0300

    Add files via upload

---
+++ vscode_14(партфолио)/js/main.js
@@ -0,0 +1,4 @@
+$('.slick__wrapper').slick({
+	prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-prev.png" alt=""></button>',
+	nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.png" alt=""></button>',
+});
